8bee2ff9c894a4877e38c72fc66e0aba1e70b032
JowettC <Jowettchng@outlook.com>
2022-12-14 17:04:15 +0800

    loader added

--- src/components/UploadSection.vue
+++ src/components/UploadSection.vue
@@ -2,14 +2,20 @@
     <div class="upload">
         <div class="container text-center">
             <div class="mb-3">
-                <input type="file" id="file-upload" class="form-control-file" @change="uploadFile()" ref="file"> <button
-                    type="button" class="btn btn-success" :disabled="!file" @click="submitFile()">Upload</button>
+                <input type="file" id="file-upload" class="form-control-file" @change="uploadFile()" ref="file" accept=".json"> <button
+                    type="button" class="btn btn-success" :disabled="!file" @click="submitFile()">Process</button>
             </div>
 
             <div class="mb-3 w-50 mx-auto">
                 <div class="alert alert-danger" role="alert" v-if="error.state">
                     {{ error.msg }}
                 </div>
+            </div>
+            <div class="mb-3">
+                <div class="loader-container" v-if="loader">
+                    <span class="loader"></span>
+                </div>
+                
             </div>
         </div>
 
@@ -27,7 +33,9 @@ export default {
             error: {
                 state: false,
                 msg: ''
-            }
+            },
+            loader: false,
+            fileData:null
         }
     },
     methods: {
@@ -44,8 +52,18 @@ export default {
             }
         },
         submitFile() {
-            console.log("emiting")
-            this.$emit('process', this.file)
+            this.loader = true;
+            this.readJsonFromFile()
+        },
+        readJsonFromFile(){
+            let reader = new FileReader();
+            reader.addEventListener("load", e => {
+                console.log(e)
+                this.fileData = JSON.parse(reader.result)
+                // console.log(this.fileData)
+                this.$emit('process', this.fileData)
+            });
+            reader.readAsText(this.file);
         }
     }
 }
