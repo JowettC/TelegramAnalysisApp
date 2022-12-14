06bbea211f401ba061c0af1e7dce4af1a9bcc280
JowettC <Jowettchng@outlook.com>
2022-12-14 15:21:55 +0800

    Create ResultSection.vue

---
+++ src/components/ResultSection.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">result section</div>
+</template>
+<script>
+export default {
+    name: 'ResultSection',
+    props: {
+        data: String,
+    },
+    components: {
+    },
+    data() {
+        return {
+        }
+    },
+    methods: {
+    },
+    mounted(){
+        console.log(this.data)
+    }
+}
+</script>
