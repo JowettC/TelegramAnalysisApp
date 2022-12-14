70c1a977db0a5bf256b87db841a44bc1f1cd379f
JowettC <Jowettchng@outlook.com>
2022-12-14 17:03:18 +0800

    aos import

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// Bootstrap Library
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Aos Library
+import 'aos/dist/aos.css'
 
 createApp(App).use(router).mount('#app')
 
