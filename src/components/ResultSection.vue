127ef7dbccf0584a9fec7e31b5bac42431a904a0
JowettC <Jowettchng@outlook.com>
2022-12-14 17:28:23 +0800

    npm i chartjs

--- src/components/ResultSection.vue
+++ src/components/ResultSection.vue
@@ -1,21 +1,25 @@
 <template>
     <div class="container">Results</div>
+    <FrequentChats :data="data['frequent_contacts']['list']"/>
 </template>
 <script>
+import FrequentChats from "@/components/charts/FrequentChats.vue"
 export default {
     name: 'ResultSection',
     props: {
         data: Object,
     },
     components: {
+        FrequentChats
     },
     data() {
         return {
         }
+            
     },
     methods: {
     },
-    mounted(){
+    mounted() {
         console.log(this.data)
     }
 }
