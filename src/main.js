1cad479a96fcd6f7198f268428868656452329df
JowettC <jowettchng@outlook.com>
2022-12-15 14:42:16 +0800

    bootstrap javascript behavior

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 // Bootstrap Library
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "bootstrap/dist/js/bootstrap.js"
 // Aos Library
 import 'aos/dist/aos.css'
 
