48a69bda409afa698e25d3b81f5dc60b8b5b69ff
JowettC <Jowettchng@outlook.com>
2022-12-22 15:40:27 +0800

    Added instructions

--- src/views/InstructionsView.vue
+++ src/views/InstructionsView.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Instructions</h1>
+    Only works with Telegram Desktop
+    
+    <div class="container mt-3">
+      <section>
+        <div class="title fw-bold">Steps:</div>
+        <div class="content">
+          Download your Telegram data from Settings -> Advanced -> Export Telegram Data
+          <br/>
+          Important: <b>Select Machine-readable JSON</b>
+          <br/>
+          Guide with Pics: <a href="https://www.maketecheasier.com/export-telegram-chat-history/#export-all-telegram-chats">https://www.maketecheasier.com/export-telegram-chat-history/#export-all-telegram-chats</a>
+          </div>
+          <br/>
+          <br/>
+          Once downloaded you can upload the file into this Web Application
+      </section>
+    </div>
   </div>
 </template>
