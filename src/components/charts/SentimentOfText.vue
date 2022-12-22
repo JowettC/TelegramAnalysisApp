f1f4b5e78aed8d22f89944d5734e2346ebc14fce
JowettC <Jowettchng@outlook.com>
2022-12-22 14:58:32 +0800

    Sentiment analysis completed

--- src/components/charts/SentimentOfText.vue
+++ src/components/charts/SentimentOfText.vue
@@ -1,14 +1,139 @@
 <template>
-<div class="container"></div>
+    <div class="container">
+        <br />
+        <div class="form-group col-12 col-sm-6 mx-auto p-2">
+            <select class="form-select selectpicker" v-model="selected" @change="changeChat" data-live-search="true">
+                <option :value="null"> Select a Chat</option>
+                <option v-for="chat in chats" :value="chat.value" :key="chat.value">{{ chat.name }}</option>
+
+            </select>
+        </div>
+        <div class="card col-12 col-sm-6 mx-auto results" v-if="selected != null">
+            <div class="title text-center"> Results</div>
+            
+            <div class="row">
+                <div class="col-12 col-sm-6">
+                    Yours
+                    <div class="content">
+                        <br />
+                        Total Message: <br />{{ result.used.length }}
+                        <br />
+                        Average Characters Per Msg: <br />{{ result.usedLength / result.used.length }}
+                        <br />
+                        Sentiment Score: <br />{{ result.usedValue/ result.used.length }}
+                    </div>
+
+                </div>
+                <div class="col-12 col-sm-6">
+                    {{ data[selected].name }}
+                    <div class="content">
+                        <br />
+                        Total Message: <br />{{ result.received.length }}
+                        <br />
+                        Average Characters Per Msg: <br />{{ result.receivedLength / result.received.length }}
+                        <br />
+                        Sentiment Score: <br />{{ result.receivedValue/ result.received.length }}
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
 </template>
 <script>
-// import {NlpManager} from "node-nlp"
 
+import Sentiment from "sentiment";
 
 export default {
-    name:"SentimentOfText",
-    mounted(){
-        
+    name: "SentimentOfText",
+    data() {
+        return {
+            sentimentAnalysis: null,
+            selected: null,
+            result: {
+                used: [],
+                usedLength: 0,
+                usedValue: 0,
+                received: [],
+                receivedLength: 0,
+                receivedValue: 0
+            },
+
+        }
+    },
+    props: {
+        data: Object
+    },
+    mounted() {
+        this.sentimentAnalysis = new Sentiment();
+    },
+    computed: {
+        chats() {
+            var res = []
+            for (var i = 0; i < this.data.length; i++) {
+                res.push(
+                    {
+                        name: this.data[i].name,
+                        value: i
+                    }
+                )
+            }
+            return res
+        },
+
+    },
+    methods: {
+        changeChat() {
+
+            this.updateValues()
+        },
+        resetValue() {
+            this.result.used = []
+            this.result.usedValue = 0
+            this.result.received = []
+            this.result.receivedValue = 0
+            this.result.usedLength = 0
+            this.result.receivedLength = 0
+        },
+        updateValues() {
+            this.resetValue()
+            for (var i = 0; i < this.data[this.selected].messages.length; i++) {
+                var message = this.data[this.selected].messages[i]
+
+                try {
+                    if (message.from.toLowerCase() == localStorage.getItem("name")) {
+                        this.result.used.push(message.text)
+                        this.result.usedLength += message.text.length
+                        this.result.usedValue += this.sentimentAnalysis.analyze(message.text).score
+                    }
+                    else {
+                        this.result.received.push(message.text)
+                        this.result.receivedLength += message.text.length
+                        this.result.receivedValue += this.sentimentAnalysis.analyze(message.text).score
+                    }
+                }
+                catch {
+                    // for special cases
+                    console.log("skipping text")
+                    continue;
+                }
+
+            }
+
+        }
+
     }
 }
 </script>
+<style scoped>
+.results {
+    min-height: 100px;
+    /* width: 100%; */
+    background-color: #f8f9fa;
+    border-radius: 5px;
+    padding: 10px;
+    margin: 10px;
+    /* text-align:left */
+}
+</style>
