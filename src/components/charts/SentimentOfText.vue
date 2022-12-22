be97e490b9900becb0eed19822b795182e15620c
JowettC <Jowettchng@outlook.com>
2022-12-22 11:29:07 +0800

    Create SentimentOfText.vue

---
+++ src/components/charts/SentimentOfText.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="container"></div>
+</template>
+<script>
+// import {NlpManager} from "node-nlp"
+
+
+export default {
+    name:"SentimentOfText",
+    async mounted(){
+        // const manager = new NlpManager({ languages: ["en"] });
+        // const result = await manager.process(
+        //     "en",
+        //     // "cero ocho cero once",
+        //     "fuck you",
+        //     {}
+        // );
+        // console.log(result)
+    }
+}
+</script>
